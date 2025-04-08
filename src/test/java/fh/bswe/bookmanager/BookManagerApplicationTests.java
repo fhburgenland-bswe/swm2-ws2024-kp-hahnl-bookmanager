9296b525190af71ee4d176f885e3225240f8ee8d
Stefan Hahnl <hahnl.stefan@gmail.com>
2025-04-08 22:18:43 +0200

    feat: initialize spring boot project; related to #4

---
+++ src/test/java/fh/bswe/bookmanager/BookManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package fh.bswe.bookmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookManagerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
