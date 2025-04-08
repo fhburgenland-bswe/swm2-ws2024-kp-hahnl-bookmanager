d6f58f639ce3d45923525601b6e3df26e353f9b4
Stefan Hahnl <hahnl.stefan@gmail.com>
2025-04-09 01:52:10 +0200

    chore: add checkstyle with configuration; related to #13

--- src/main/java/fh/bswe/bookmanager/BookManagerApplication.java
+++ src/main/java/fh/bswe/bookmanager/BookManagerApplication.java
@@ -3,9 +3,16 @@ package fh.bswe.bookmanager;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * The main entry point for the Book Manager application.
+ */
 @SpringBootApplication
 public class BookManagerApplication {
 
+    /**
+     * The main method that starts the Spring Boot application.
+     * @param args  command-line arguments passed to the application
+     */
     public static void main(String[] args) {
         SpringApplication.run(BookManagerApplication.class, args);
     }
