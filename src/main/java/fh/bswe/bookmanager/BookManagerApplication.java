d1ba0c80c09cfd83951ab0b85a3ed8b712c085d3
Stefan Hahnl <hahnl.stefan@gmail.com>
2025-04-09 13:53:00 +0200

    fix: change ruleset file path config; related to #17

--- src/main/java/fh/bswe/bookmanager/BookManagerApplication.java
+++ src/main/java/fh/bswe/bookmanager/BookManagerApplication.java
@@ -13,7 +13,7 @@ public class BookManagerApplication {
      * The main method that starts the Spring Boot application.
      * @param args  command-line arguments passed to the application
      */
-    public static void main(String[] args) {
+    public static void main(final String[] args) {
         SpringApplication.run(BookManagerApplication.class, args);
     }
 
